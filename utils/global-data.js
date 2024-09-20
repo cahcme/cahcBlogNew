a92b699e58f7e49ed33600179ec435cda2c2a6aa
cahcme <cahcme@gmail.com>
2024-09-21 01:27:11 +0200

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,10 +4,14 @@ export const getGlobalData = () => {
     : 'Caнc';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Cybersecurity Blog';
+    : 'Blog de ciberseguridad';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
+
+  const introduction = process.env.BLOG_INTRODUCTION
+    ? decodeURI(process.env.BLOG_INTRODUCTION)
+    : 'Soy Caнc, un profesional de ciberseguridad de España. Me especializo en realizar pruebas de penetración y evaluación de vulnerabilidades, utilizando herramientas como Metasploit, Nmap, Wireshark entre muchas otras. Actualmente, soy Tester de Penetración y Analista de Seguridad, donde colaboro con los equipos de desarrollo y operaciones para identificar brechas de seguridad y aplicar soluciones efectivas.';
 
   return {
     name,
