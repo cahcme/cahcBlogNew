c0db3c7bde412c698916e966555875f3200c62bc
cahcme <cahcme@gmail.com>
2024-09-21 02:20:24 +0200

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -8,9 +8,8 @@ export const getGlobalData = () => {
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
-  const introduction = process.env.BLOG_INTRODUCTION
-    ? decodeURI(process.env.BLOG_INTRODUCTION)
-    : 'Soy Caнc, un profesional de ciberseguridad de España. Me especializo en realizar pruebas de penetración y evaluación de vulnerabilidades, utilizando herramientas como Metasploit, Nmap, Wireshark entre muchas otras. Actualmente, soy Tester de Penetración y Analista de Seguridad, donde colaboro con los equipos de desarrollo y operaciones para identificar brechas de seguridad y aplicar soluciones efectivas.';
+   const introduction = process.env.BLOG_INTRODUCTION ? decodeURI(process.env.BLOG_INTRODUCTION) : 'Soy Caнc, un profesional de ciberseguridad de España. Me especializo en realizar pruebas de penetración y evaluación de vulnerabilidades, utilizando herramientas como Metasploit, Nmap, Wireshark entre muchas otras. Actualmente, soy Tester de Penetración y Analista de Seguridad, donde colaboro con los equipos de desarrollo y operaciones para identificar brechas de seguridad y aplicar soluciones efectivas.';
+    console.log('Introducción:', introduction);
 
   return {
     name,
