da5aadc0cc01aee0623eb7c115685d4c3c3310d6
cahcme <cahcme@gmail.com>
2024-09-21 16:30:59 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,12 +6,18 @@ export default function Header({ name, introduction }) {
       <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">
-          {name}
+          <a>{name}</a>
         </Link>
       </p>
-      <p className="text-2xl text-center dark:text-white">
-        {introduction}  {/* Aquí se muestra la introducción */}
+      <p className="text-lg text-center dark:text-gray-400 mt-2">
+        {introduction}
       </p>
+      {/* Añadimos el enlace a la página de "Sobre mí" */}
+      <nav className="text-center mt-4">
+        <Link href="/about">
+          <a className="text-lg dark:text-gray-400 hover:underline">Sobre mí</a>
+        </Link>
+      </nav>
     </header>
   );
 }
